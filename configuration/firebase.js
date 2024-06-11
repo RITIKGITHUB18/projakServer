6901db73fafee89a226c95f64e45a472ca1a0c5b
Ritik kumar <94630724+RITIKGITHUB18@users.noreply.github.com>
2024-06-12 01:05:04 +0530

    Update firebase.js

--- configuration/firebase.js
+++ configuration/firebase.js
@@ -1,45 +1,10 @@
-// const dotenv = require("dotenv");
-// const assert = require("assert");
-// require("dotenv").config();
-
-// const {
-//   PORT,
-//   HOST,
-//   HOST_URL,
-//   API_KEY,
-//   AUTH_DOMAIN,
-//   PROJECT_ID,
-//   FIRESTORE_DB_URL,
-//   STORAGE_BUCKET,
-//   MESSAGING_SENDER_ID,
-//   APP_ID,
-//   MEASUREMENT_ID,
-// } = process.env;
-
-// assert(PORT, "PORT is required");
-// assert(HOST, "HOST is required");
-
-// module.exports = {
-//   port: PORT,
-//   host: HOST,
-//   url: HOST_URL,
-//   firebaseConfig: {
-//     apiKey: API_KEY,
-//     authDomain: AUTH_DOMAIN,
-//     projectId: PROJECT_ID,
-//     storageBucket: STORAGE_BUCKET,
-//     messagingSenderId: MESSAGING_SENDER_ID,
-//     appId: APP_ID,
-//     measurementId: MEASUREMENT_ID,
-//   },
-// };
 
 const admin = require("firebase-admin");
 const dotenv = require("dotenv");
-const serviceAccount = require("../key.json");
+// const serviceAccount = require("../key.json");
 
 dotenv.config();
-
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
